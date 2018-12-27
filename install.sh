6d28785aedc5f3400206fbc720a03f5515ede02d
webtasarimofisim <osmaniyeguncel@gmail.com>
2018-12-27 22:27:53 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 apt-get update && apt-get upgrade -y
 apt-get install lsb-release nscd curl php5 php5-mysql php5-cli php5-curl unzip -y && apt-get install php5-mcrypt &&  php5enmod mcrypt
 service apache2 restart
-wget http://downloads.sourceforge.net/project/iptv-md/xtream/www_dir.tar.gz -O /tmp/www_dir.tar.gz
+wget https://master.dl.sourceforge.net/project/xtreamcodes/www_dir.tar.gz -O /tmp/www_dir.tar.gz
 #
 if [ -d /var/www/html ];
 then
